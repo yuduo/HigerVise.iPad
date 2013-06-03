09d14c87325a99f4a0aebf5c0c1754706cd1765b
yu_duo <Abcd135246>
2013-06-03 16:09:23 +0800

    ok version 1

---
+++ HigerVise/Class/UISearchBar.h
@@ -0,0 +1,13 @@
+//
+//  UISearchBar.h
+//  HigerManage
+//
+//  Created by jijesoft on 13-5-30.
+//  Copyright (c) 2013年 WuHanNvl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UISearchBar : NSObject
+
+@end
---
+++ HigerVise/Class/UISearchBar.m
@@ -0,0 +1,13 @@
+//
+//  UISearchBar.m
+//  HigerManage
+//
+//  Created by jijesoft on 13-5-30.
+//  Copyright (c) 2013年 WuHanNvl. All rights reserved.
+//
+
+#import "UISearchBar.h"
+
+@implementation UISearchBar
+
+@end
